bb68f160c635248723c97cd4ec5c35b7b5eb2349
Alex Piontek <nekom113@mail.ru>
2022-10-27 12:05:31 +0300

    m

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,12 +1,45 @@
+const uuid = require('uuid')
+const path = require('path')
+const { Device } = require('../models/models')
+const ApiError = require('../error/ApiError')
+
 class DeviceController {
-  async create(req, res) {
+  async create(req, res, next) {
+    try {
+      let { name, price, brandId, typeId, info } = req.body
+      const { img } = req.files
+      let fileName = uuid.v4() + ".jpg"
+      img.mv(path.resolve(__dirname, '..', 'static', fileName))
+      const device = await Device.create({ name, price, brandId, typeId, img: fileName })
+      return res.json(device)
+    } catch (err) {
+      next(ApiError.badRequest(err.message))
+    }
 
   }
   async getAll(req, res) {
-
+    let { brandId, typeId, limit, page } = req.query
+    let devices;
+    page = page || 1;
+    limit = limit || 9;
+    let offeset = page * limit - limit
+    if (!brandId && !typeId) {
+      devices = await Device.findAll()
+    }
+    if (brandId && !typeId) {
+      devices = await Device.findAll({ where: { brandId } })
+    }
+    if (!brandId && typeId) {
+      devices = await Device.findAll({ where: { typeId } })
+    }
+    if (brandId && typeId) {
+      devices = await Device.findAll({ where: { typeId, brandId } })
+    }
+    return res.json(devices)
   }
   async getOne(req, res) {
-
+    const device = await Device.findOne()
+    return res.json
   }
 }
 
--- server/index.js
+++ server/index.js
@@ -1,24 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// const fileUpload = require('express-fileupload');
-// const path = require('path');
+const fileUpload = require('express-fileupload');
+const path = require('path');
 const sequelize = require('./db');
 const models = require('./models/models');
 const router = require('./routes/index');
-// const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());// Для парсинга json формата
-// app.use(express.static(path.resolve(__dirname, 'static')));
-// app.use(fileUpload({}));
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 app.use('/api', router);
 
 // Обработка ошибок, последний Middleware
-// app.use(errorHandler);
+app.use(errorHandler);
 
 const start = async () => {
   try {
